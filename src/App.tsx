38dec59da20d7aed711fadd89c39c7903ea0a671
liulongbin <liulongbin1314@outlook.com>
2023-09-11 18:58:08 +0800

    以函数的形式为useState赋初始值

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Count } from '@/components/use_state/01.base.tsx'
+import { DateCom } from '@/components/use_state/01.base.tsx'
 
 // React.FC<T> 表示这是一个函数式的 react 组件
 const App: React.FC = () => {
   return (
     <>
-      <Count />
+      <DateCom />
     </>
   )
 }
--- src/components/use_state/01.base.tsx
+++ src/components/use_state/01.base.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 export const Count: React.FC = () => {
   const [count, setCount] = useState(0)
 
+  console.log('触发了 Count 组件的渲染')
+
   const add = () => {
     setCount(count + 1)
   }
@@ -11,6 +13,23 @@ export const Count: React.FC = () => {
     <>
       <h1>count 的值是：{count}</h1>
       <button onClick={add}>+1</button>
+    </>
+  )
+}
+
+export const DateCom: React.FC = () => {
+  // const [date] = useState({ year: 2023, month: 9, day: 11 })
+  const [date] = useState(() => {
+    const dt = new Date()
+    return { year: dt.getFullYear(), month: dt.getMonth() + 1, day: dt.getDate() }
+  })
+
+  return (
+    <>
+      <h1>当前的年月日信息：</h1>
+      <p>年份：{date.year}年</p>
+      <p>月份：{date.month}月</p>
+      <p>日期：{date.day}日</p>
     </>
   )
 }
