1087d0cff073092c41247e2e6acd72e04e6bbfc4
liulongbin <liulongbin1314@outlook.com>
2023-09-12 13:42:57 +0800

    基于ref实现组件渲染周期之间的数据共享

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { InputFocus } from '@/components/use_ref/01.base.tsx'
+import { Counter } from '@/components/use_ref/01.base.tsx'
 
 // React.FC<T> 表示这是一个函数式的 react 组件
 const App: React.FC = () => {
   return (
     <>
-      <InputFocus />
+      <Counter />
     </>
   )
 }
--- src/components/use_ref/01.base.tsx
+++ src/components/use_ref/01.base.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 export const InputFocus: React.FC = () => {
   const iptRef = useRef<HTMLInputElement>(null)
@@ -12,6 +12,46 @@ export const InputFocus: React.FC = () => {
     <>
       <input type="text" ref={iptRef} />
       <button onClick={getFocus}>获取焦点</button>
+    </>
+  )
+}
+
+/* let prevCount: number
+export const Counter: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  const add = () => {
+    setCount((prev) => prev + 1)
+    prevCount = count
+  }
+
+  return (
+    <>
+      <h1>
+        新值是：{count}；旧值是：{prevCount}
+      </h1>
+      <button onClick={add}>+1</button>
+    </>
+  )
+} */
+
+export const Counter: React.FC = () => {
+  const [count, setCount] = useState(0)
+  // useRef() 只在组件首次渲染的时候被创建
+  // 如果组件是 rerender 的时候，不会重复创建 ref 对象
+  const prevCountRef = useRef<number>()
+
+  const add = () => {
+    setCount((prev) => prev + 1)
+    prevCountRef.current = count
+  }
+
+  return (
+    <>
+      <h1>
+        新值是：{count}；旧值是：{prevCountRef.current}
+      </h1>
+      <button onClick={add}>+1</button>
     </>
   )
 }
