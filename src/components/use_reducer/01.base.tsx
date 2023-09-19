a6da4c848735111d746bf632194236a402fe7d59
liulongbin <liulongbin1314@outlook.com>
2023-09-19 22:52:13 +0800

    使用initAction

--- src/components/use_reducer/01.base.tsx
+++ src/components/use_reducer/01.base.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+type UserType = typeof defaultState
+
+// 初始状态
+const defaultState = { name: 'liulongbin', age: 0 }
+
+// 在 reducer 函数的形参中：
+// 第一个参数，永远都是上一次的旧状态
+const reducer = (prevState: UserType) => {
+  console.log('触发了 reducer 的执行')
+  // 在 reducer 函数中，必须向外返回一个处理好的新状态
+  return prevState
+}
+
+// 形参：是初始状态
+// 返回值：处理好的初始状态
+const initAction = (initState: UserType) => {
+  return { ...initState, age: Math.round(Math.abs(initState.age)) || 18 }
+}
 
 export const Father: React.FC = () => {
+  const [state] = useReducer(reducer, defaultState, initAction)
+
+  console.log(state)
+
   return (
     <div>
       <button>修改用户名</button>
