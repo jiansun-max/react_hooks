a13615b4a49b4af87e80343e615b217fc917bbe1
liulongbin <liulongbin1314@outlook.com>
2023-09-20 14:08:17 +0800

    使用Immer编写更简洁的reducer更新逻辑

--- src/components/use_reducer/01.base.tsx
+++ src/components/use_reducer/01.base.tsx
@@ -1,7 +1,8 @@
-import React, { useReducer } from 'react'
+import React from 'react'
+import { useImmerReducer } from 'use-immer'
 
 type UserType = typeof defaultState
-type ActionType = { type: 'UPDATE_NAME'; payload: string } | { type: 'INCREMENT'; payload: number } | { type: 'DECREMENT'; payload: number }
+type ActionType = { type: 'UPDATE_NAME'; payload: string } | { type: 'INCREMENT'; payload: number } | { type: 'DECREMENT'; payload: number } | { type: 'RESET' }
 
 // 初始状态
 const defaultState = { name: 'liulongbin', age: 0 }
@@ -14,11 +15,20 @@ const reducer = (prevState: UserType, action: ActionType) => {
 
   switch (action.type) {
     case 'UPDATE_NAME':
-      return { ...prevState, name: action.payload }
+      // return { ...prevState, name: action.payload }
+      prevState.name = action.payload
+      break
     case 'INCREMENT':
-      return { ...prevState, age: prevState.age + action.payload }
+      // return { ...prevState, age: prevState.age + action.payload }
+      prevState.age += action.payload
+      break
     case 'DECREMENT':
-      return { ...prevState, age: prevState.age - action.payload }
+      // return { ...prevState, age: prevState.age - action.payload }
+      prevState.age -= action.payload
+      break
+    case 'RESET':
+      // return { ...defaultState, age: defaultState.age === 0 ? 18 : defaultState.age }
+      return initAction(defaultState)
     default:
       return prevState
   }
@@ -31,7 +41,7 @@ const initAction = (initState: UserType) => {
 }
 
 export const Father: React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, defaultState, initAction)
+  const [state, dispatch] = useImmerReducer(reducer, defaultState, initAction)
 
   console.log(state)
 
@@ -77,6 +87,19 @@ const Son2: React.FC<UserType & { dispatch: React.Dispatch<ActionType> }> = (pro
     <div className="son2">
       <p>{JSON.stringify(user)}</p>
       <button onClick={sub}>年龄-5</button>
+      <hr />
+      <GrandSon dispatch={dispatch} />
     </div>
+  )
+}
+
+const GrandSon: React.FC<{ dispatch: React.Dispatch<ActionType> }> = (props) => {
+  const reset = () => props.dispatch({ type: 'RESET' })
+
+  return (
+    <>
+      <h3>这是 GrandSon 组件</h3>
+      <button onClick={reset}>重置</button>
+    </>
   )
 }
